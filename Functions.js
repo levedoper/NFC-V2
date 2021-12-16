bb6433def8f33b379dedd026341d74c2b0654fbf
levedoper <ricardoferegrinojr@gmail.com>
2021-12-15 20:51:16 -0600

    commit

--- Functions.js
+++ Functions.js
@@ -1,6 +1,10 @@
-resizeHeight = (input) => {
-    sWidth = window.innerWidth;
-    sHeight = window.innerHeight;
-    document.getElementById(input).style.height = `${sHeight}px`;
-    document.getElementById(input).innerHTML = sHeight;
+const adjustObjHeight = (obj) => {
+    document.getElementById(obj).style.height = `${window.innerHeight}px`;
+};
+const adjustTopMargin = (obj, center, porcentage) => {
+    let portopix = porcentage * window.innerHeight;
+    document.getElementById(obj).style.marginTop = `${portopix-center}px`
+};
+const setRelHeight = (obj,height) => {
+    document.getElementById(obj).style.height = `${height*window.innerHeight}px`;
 };
